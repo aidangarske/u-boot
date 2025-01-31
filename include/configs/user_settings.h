6a5411d28cbaa46fabfd24dab1a524d20eaeaca2
aidan garske <aidan@wolfssl.com>
2025-01-30 20:25:52 -0800

    added wolfTPM as a gitmodule / added user_settings.h and fixed include paths

---
+++ include/configs/user_settings.h
@@ -0,0 +1,48 @@
+/* user_settings.h
+ *
+ * Copyright (C) 2006-2025 wolfSSL Inc.
+ *
+ * This file is part of wolfSSL.
+ *
+ * wolfSSL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
+ */
+
+
+#ifndef USER_SETTINGS_H
+#define USER_SETTINGS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#include <linux/types.h>
+
+#define XSLEEP_MS(ms) udelay(ms * 1000)
+
+#define XTPM_WAIT() /* just poll without delay by default */
+
+/* U-boot with wolfTPM specific macros */
+
+#define WOLFTPM_AUTODETECT
+
+#define WOLFTPM_FIRMWARE_UPGRADE
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* USER_SETTINGS_H */
