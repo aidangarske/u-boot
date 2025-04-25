b89387d04dd4991faa4783967460baebd415451b
David Garske <david@wolfssl.com>
2025-04-25 12:20:51 -0700

    Add support for wolfTPM 2.0 library. Adds new option to use wolfTPM and has ports for all of the existing tpm commands. Also add firmware update support for Infineon SLB9672 and SLB9673 TPM 2.0 modules. Co-authored-by: Aidan Garske <aidan@wolfssl.com>

---
+++ include/configs/user_settings.h
@@ -0,0 +1,63 @@
+/* user_settings.h
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ * (C) Copyright 2025
+ * Aidan Garske <aidan@wolfssl.com>
+ */
+
+#ifndef USER_SETTINGS_H
+#define USER_SETTINGS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************/
+/* --- BEGIN wolfTPM U-boot Settings -- */
+/******************************************************************************/
+
+/* Firmware upgrade only supported
+ * by infineon SLB9672/SLB9673 use
+ * this for now */
+#define WOLFTPM_SLB9672
+
+/* Include delay.h and types.h for
+ * U-boot time delay and types */
+#include <linux/delay.h>
+#include <linux/types.h>
+
+/* No wolfCrypt dependency */
+#undef  WOLFTPM2_NO_WOLFCRYPT
+#define WOLFTPM2_NO_WOLFCRYPT
+
+/* For U-boot with Linux device */
+#define WOLFTPM_LINUX_DEV
+
+#define XSLEEP_MS(ms) udelay(ms * 1000)
+
+/* Just poll without delay by default */
+#define XTPM_WAIT()
+
+#define WOLFTPM_AUTODETECT
+#define WOLFTPM_FIRMWARE_UPGRADE
+
+/* Do not include API's that use heap(), they are not required */
+#define WOLFTPM2_NO_HEAP
+
+/* Debugging */
+#if 1
+    #define DEBUG_WOLFTPM
+    #define WOLFTPM_DEBUG_VERBOSE
+    #define WOLFTPM_DEBUG_IO
+#endif
+
+/******************************************************************************/
+/* --- END wolfTPM U-boot Settings -- */
+/******************************************************************************/
+
+#ifdef __cpluspluss
+}
+#endif
+
+#endif /* USER_SETTINGS_H */
